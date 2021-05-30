843e62aa21d5562ee645010b454eb6788fe36958
lomotos10 <lomotos10@gmail.com>
2021-05-31 01:28:35 +0900

    merge main

--- src/gCurve2D.cpp
+++ src/gCurve2D.cpp
@@ -1,3 +1,5 @@
+#include <ctime>
+#include <fstream>
 #include <iomanip>
 
 #include "gDel2D/CpuCurve.h"
@@ -10,14 +12,24 @@
 #include "DelaunayChecker.h"
 #include "InputCreator.h"
 
-int main(int argc, char *argv[]) {
-
+void run(int num_points) {
   GCurve2DInput g_input;
   GCurve2DOutput g_output;
   GpuCurve gpuCurve;
 
   InputCreator g_creator;
   g_creator.makePoints(1000000, DiskDistribution, g_input.pointVec, 76213898);
+  std::ifstream fin;
+  fin.open("../src/data/donut2.xy");
+  if (!fin) { // file couldn't be opened
+    std::cerr << "Error: file could not be opened" << std::endl;
+    exit(1);
+  }
+  do {
+    double x, y;
+    fin >> x >> y;
+    g_input.pointVec.push_back(Point2{{x, y}});
+  } while (!fin.eof());
   gpuCurve.compute(g_input, &g_output);
 
   std::cout << "done1" << std::endl;
@@ -28,4 +40,15 @@ int main(int argc, char *argv[]) {
   CpuCurve_compute(c_input);
 
   std::cout << "done2" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  /*
+  int num_points[] = {50, 100, 500, 1000, 5000, 10000, 50000};
+  for (int i = 0; i < sizeof num_points / sizeof(int); i++) {
+    std::cout << num_points[i] << " points:" << std::endl;
+    run(num_points[i]);
+  }*/
+  run(0);
+  return 0;
 }
