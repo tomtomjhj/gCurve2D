242d218f1ab43680dc8baac910bccec02a72942a
lomotos10 <lomotos10@gmail.com>
2021-05-31 14:57:54 +0900

    visualize and performance test

--- src/examples/otr2_example.cpp
+++ src/examples/otr2_example.cpp
@@ -15,30 +15,28 @@ typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_2 Point;
 typedef K::Segment_2 Segment;
 typedef CGAL::Optimal_transportation_reconstruction_2<K> Otr;
-int main() {
+
+void run(int test_points) {
   // Generate a set of random points on the boundary of a square.
   std::vector<Point> points;
+  points.reserve(test_points);
 
   // insert using point generator
   CGAL::Random_points_on_square_2<Point> point_generator(1.);
-  std::copy_n(point_generator, 100, std::back_inserter(points));
-
-  // insert points one by one
-  points.push_back(Point(0, 0));
-  points.push_back(Point(1, 0));
-  points.push_back(Point(0, 1));
+  std::copy_n(point_generator, test_points, std::back_inserter(points));
 
   CGAL::Real_timer timer{};
   timer.start();
 
   Otr otr(points);
+  otr.set_verbose(1);
   timer.stop();
   double constr_time = timer.time();
   std::cout << "Construction time: " << constr_time << " seconds" << std::endl;
   timer.reset();
   timer.start();
 
-  if (otr.run(50)) // 50 steps
+  if (otr.run_until(4)) // 50 steps
     std::cerr << "All done." << std::endl;
   else
     std::cerr << "Premature ending." << std::endl;
@@ -48,7 +46,6 @@ int main() {
   std::cout << "Otr run time:      " << timer.time() << " seconds" << std::endl;
   std::cout << "Total time:        " << constr_time + otr_time << " seconds"
             << std::endl;
-  std::cout << std::endl;
 
   // list output
   // https://doc.cgal.org/latest/Optimal_transportation_reconstruction_2/classCGAL_1_1Optimal__transportation__reconstruction__2.html#ad94fc673df480c8b2076ae542fdd0e04
@@ -58,18 +55,35 @@ int main() {
   otr.list_output(std::back_inserter(isolated_points),
                   std::back_inserter(segments));
 
-  // Point: https://doc.cgal.org/latest/Kernel_23/classCGAL_1_1Point__2.html
-  std::vector<Point>::iterator pit;
-  for (pit = isolated_points.begin(); pit != isolated_points.end(); pit++)
-    std::cout << *pit << std::endl;
-
+  std::cout << "Isolated points: " << isolated_points.size()
+            << ", Edges: " << segments.size() << std::endl;
   std::cout << std::endl;
 
-  // Segment: https://doc.cgal.org/latest/Kernel_23/classCGAL_1_1Segment__2.html
-  std::vector<Segment>::iterator sit;
-  for (sit = segments.begin(); sit != segments.end(); sit++)
-    std::cout << *sit << std::endl;
-  std::cout << std::endl;
+  bool output_data = false;
+  if (output_data) {
+
+    // Point: https://doc.cgal.org/latest/Kernel_23/classCGAL_1_1Point__2.html
+    std::vector<Point>::iterator pit;
+    for (pit = isolated_points.begin(); pit != isolated_points.end(); pit++)
+      std::cout << *pit << std::endl;
+
+    std::cout << std::endl;
+
+    // Segment:
+    // https://doc.cgal.org/latest/Kernel_23/classCGAL_1_1Segment__2.html
+    std::vector<Segment>::iterator sit;
+    for (sit = segments.begin(); sit != segments.end(); sit++)
+      std::cout << *sit << std::endl;
+    std::cout << std::endl;
+  }
+}
+
+int main() {
+  int num_points[] = {50, 100, 500, 1000, 5000, 10000, 50000};
+  for (int i = 0; i < sizeof num_points / sizeof(int); i++) {
+    std::cout << num_points[i] << " points:" << std::endl;
+    run(num_points[i]);
+  }
 
   return 0;
 }
--- src/gDel2D/CPU/CpuCurve.cpp
+++ src/gDel2D/CPU/CpuCurve.cpp
@@ -1,6 +1,7 @@
 #include "../CpuCurve.h"
 
 #include <algorithm>
+#include <ctime>
 #include <iomanip>
 #include <iostream>
 #include <vector>
@@ -14,18 +15,32 @@
 SegmentVec CpuCurve_compute(const Point2Vec &input) {
 
   // Let V be the vertices of the Voronoi diagram of S.
+  clock_t time;
+  time = clock();
   std::unordered_set<CPoint> s_points(input.begin(), input.end());
+  std::cout << ((float)(clock() - time)) / CLOCKS_PER_SEC
+            << " seconds \t s_points constructor" << std::endl;
+  time = clock();
   VD v_VD{s_points.begin(), s_points.end()};
+  std::cout << ((float)(clock() - time)) / CLOCKS_PER_SEC
+            << " seconds \t VD constructor" << std::endl;
 
   // Let D be the Delaunay triangulation of S∪V.
   Point2Vec sv_points;
+  time = clock();
   sv_points.insert(sv_points.end(), s_points.begin(), s_points.end());
   for (auto i = v_VD.vertices_begin(); i != v_VD.vertices_end(); i++) {
     sv_points.push_back(i->point());
   }
+  std::cout << ((float)(clock() - time)) / CLOCKS_PER_SEC
+            << " seconds \t sv_points insert" << std::endl;
+  time = clock();
   DT sv_DT{sv_points.begin(), sv_points.end()};
+  std::cout << ((float)(clock() - time)) / CLOCKS_PER_SEC
+            << " seconds \t DT constructor" << std::endl;
 
   // An edge of D belongs to the crust of S if both its endpoints belong to S
+  time = clock();
   SegmentVec crust_segements;
   for (auto i = sv_DT.all_edges_begin(); i != sv_DT.all_edges_end(); i++) {
     auto face = i->first;
@@ -38,5 +53,11 @@ SegmentVec CpuCurve_compute(const Point2Vec &input) {
       crust_segements.push_back(
           std::make_pair(source->point(), target->point()));
   }
-  return std::move(crust_segements);
+  std::cout << ((float)(clock() - time)) / CLOCKS_PER_SEC << " seconds \t crust"
+            << std::endl;
+  time = clock();
+  auto x = std::move(crust_segements);
+  std::cout << ((float)(clock() - time)) / CLOCKS_PER_SEC
+            << " seconds \t move(crust_segments)" << std::endl;
+  return x;
 }
